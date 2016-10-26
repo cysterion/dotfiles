abe475c681d3d01d12579d01010f39eb52d2aa27
Brandon Roehl <BrandonRoehl@gmail.com>
2016-10-26 15:24:26 -0500

    moving to sub modules

---
+++ clone.sh
@@ -0,0 +1,5 @@
+git submodule add -f https://github.com/scrooloose/nerdcommenter.git vim/bundle/nerdcommenter
+git submodule add -f https://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree
+git submodule add -f https://github.com/scrooloose/syntastic.git vim/bundle/syntastic
+git submodule add -f https://github.com/vim-airline/vim-airline.git vim/bundle/vim-airline
+git submodule add -f https://github.com/mhinz/vim-signify.git vim/bundle/vim-signify
