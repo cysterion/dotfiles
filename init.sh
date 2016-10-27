d481beffcf6c27c6de75b6e940d02ad00ea42557
Brandon Roehl <BrandonRoehl@gmail.com>
2016-10-26 23:08:27 -0500

    Echos to verbalise what's happening

--- init.sh
+++ init.sh
@@ -5,13 +5,16 @@ DATE_STRING=$(echo "$(date)" | tr -s ' ' | tr ' ' '_')
 FILES=( "zshrc" "bashrc" "vimrc" "vim" "tmux.conf" "profile" )
 for i in "${FILES[@]}"
 do
+    echo ""
     if [ -e "$HOME/.${i}" ]
     then
-        mv -f "$HOME/.${i}" "$HOME/${i}_${DATE_STRING}"
+        echo "Moving current .${i}"
+        mv -iv "$HOME/.${i}" "$HOME/${i}_${DATE_STRING}"
     fi
+    echo "Simlinking ${i} to $HOME"
     ln -s "$PWD/${i}" "$HOME/.${i}"
     if [ $? -eq 0 ]; then
-        echo "Installed: $HOME/.${i}"
+        echo "$PWD/${i} ~> $HOME/.${i}"
     else
         echo "Install failed to symlink. See GitHub.com/BrandonRoehl/dotfiles for help"
         exit 1
