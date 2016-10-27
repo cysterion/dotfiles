20f42c063518ac7e8541277baea6bf6c1cc53175
Brandon Roehl <BrandonRoehl@gmail.com>
2016-10-26 22:20:52 -0500

    Simlinks for all

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 git submodule init
 git submodule update --recursive
 DATE_STRING=$(echo "$(date)" | tr -s ' ' | tr ' ' '_')
-FILES=( "zshrc" "bashrc" "vimrc" "vim" )
+FILES=( "zshrc" "bashrc" "vimrc" "vim" "tmux.conf" "profile" )
 for i in "${FILES[@]}"
 do
     if [ -e "$HOME/.${i}" ]
