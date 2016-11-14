d90b2e54980ea93c50d79ce93b35ae44c325b21e
Brandon Roehl <BrandonRoehl@gmail.com>
2016-11-14 13:37:52 -0600

    Update init script

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 git submodule init
 git submodule update --recursive
 DATE_STRING=$(echo "$(date)" | tr -s ' ' | tr ' ' '_')
-FILES=( "zshrc" "bashrc" "vimrc" "vim" "zsh" "tmux.conf" "profile" )
+FILES=( "zshrc" "bashrc" "vimrc" "vim" "tmux.conf" "profile" )
 for i in "${FILES[@]}"
 do
     echo ""
